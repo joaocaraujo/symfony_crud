82e93131ec3a1cb01a9b5a89007e0f3f94e59bd7
João Araujo <araujo_joaol@yahoo.com.br>
2023-02-10 11:33:05 -0300

    chore: database manipulation with doctrine

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,12 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Art;
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Registry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
 
 /**
- * Route("/", name="web_")
+ * @Route("/", name="web_")
  */
 class UserController extends AbstractController
 {
@@ -23,9 +29,24 @@ class UserController extends AbstractController
      * @Route("/save", methods={"POST"}, name="save")
      */
 
-    public function save(): Response
+    public function save(Request $request, PersistenceManagerRegistry $doctrine): Response
     {
-        return new Response("saving");
+        $data = $request->request->all();
+
+        $user = new User();
+        $user->setName($data['name']);
+        $user->setEmail($data['email']);
+        $user->setPassword($data['password']);
+
+        $em = $doctrine->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        if($user->getId())
+        {
+            return $this->render("user/success.html.twig");
+        } else {
+            return $this->render("user/erro.html.twig");
+        }
     }
 }
-
